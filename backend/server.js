3a368d7a5d2a7ea60ebded13e740bacbc9f380c6
Kevin Chen <rchen@bayviewglen.ca>
2025-04-25 15:05:40 -0400

    added some backend folders and packages

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000; 
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+//Routes 
+app.use('/api', ); // add routes here
+
+//Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
