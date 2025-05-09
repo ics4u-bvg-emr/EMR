7c7d867b4e31f1316fdf8f4adbc97d7bd9faaf7c
Kevin Chen <rchen@bayviewglen.ca>
2025-05-09 13:18:26 -0400

    created all CRUD endpoints for appointments. and it works on postman!

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 //Routes 
 app.use('/api', routes);
