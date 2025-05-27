facd8116b8ec01031b241ff6cea1f70442248c99
Parihsz <kudzuqwerty@gmail.com>
2025-05-27 14:44:16 -0400

    fix eric skidding doctor.js

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 
+const contactInfoSchema=new mongoose.Schema({
+    phoneNumber:{type:String},
+    email:{type:String},
+    address:{type:String}
+})
+
 const doctorSchema=new mongoose.Schema({
     firstName:{type:String,required:true},
     lastName:{type:String,required:true},
     specialization:{type:String},
     contact:contactInfoSchema,
-})
-
-const contactInfoSchema=new mongoose.Schema({
-    phoneNumber:{type:String},
-    email:{type:String},
-    address:{type:String}
 })
 
 const Doctor=mongoose.model('Doctor',doctorSchema)
